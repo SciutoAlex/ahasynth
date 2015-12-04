afdbd035d016f13a754e209ea824eb7829645784
SciutoAlex <SciutoAlex@gmail.com>
2015-12-04 09:56:04 -0500

    added in layout menu and custom layout save

--- webapp/js/note.js
+++ webapp/js/note.js
@@ -1,18 +1,25 @@
-var Note = function(noteObj) {
+var Note = function(noteObj, vizApp, i) {
+	var index = i;
 	var sourceHTMLTemplate   = $("#note-template").html();
 	var vizContainer   = $(".viz > div");
+	var noteObj = noteObj;
+	var customPositionData = [ vizContainer.height() * Math.random(),vizContainer.width() * Math.random()];
 	
 	noteTemplate = Handlebars.compile(sourceHTMLTemplate);
 	
-	noteEl = $("<div>", {
+	var noteEl = $("<div>", {
 		html : noteTemplate(noteObj)
 	})
-		.draggable({scroll: true })
-		.appendTo(vizContainer)
-		.css({"top" : vizContainer.height() * Math.random(),
-			   "left" : vizContainer.width() * Math.random()
-			})
-	
+	.draggable({
+		scroll: true,
+		start: dragged
+	})
+	.appendTo(vizContainer)
+	.css({
+		"top" : customPositionData[0],
+		"left" : customPositionData[1]
+	});
+		
 	var sourceEl = noteEl.find('.source').hide();
 	
 	noteEl.find('h4').css('font-size', calculatFontSize(noteObj.importance));
@@ -31,13 +38,54 @@ var Note = function(noteObj) {
 		noteEl.find('.note').css('background', color)
 	}
 	
+	var movePosition = function(arr) {
+		noteEl.animate({
+			left: arr[0],
+			top: arr[1]
+		});
+	}
+	
+	var returnSize = function() {
+		return [noteEl.width(), noteEl.height()];
+	}
+	
+	var getNoteAttr = function(attr) {
+		console.log(attr)
+		console.log(noteObj[attr])
+		return noteObj[attr];
+	}
+	
+	var customPositionGetSet = function(arr) {
+		if (typeof arr == 'undefined') {
+			return customPositionData;
+		} else {
+			console.log(arr)
+			customPositionData = arr;
+		}
+	}
+	
+	var positionGetSet = function(arr) {
+		if (typeof arr == 'undefined') {
+			return [noteEl.css('left'),noteEl.css('top')];
+		} else {
+			movePosition(arr);
+		}
+	}
+	
+	function dragged() {
+		vizApp.showSaveLayoutButton();
+	}
+	
 	return {
-		setBackground :setBackground
+		setBackground :setBackground,
+		getNoteAttr : getNoteAttr,
+		customPosition : customPositionGetSet,
+		position : positionGetSet
 	}
 	
 }
 
 function calculatFontSize(importance) {
-	var fontSize = [14,16,21,24];
+	var fontSize = [14,16,18,20];
 	return fontSize[importance];
 }
--- webapp/js/visualization.js
+++ webapp/js/visualization.js
@@ -1,14 +1,19 @@
 var visualization = function() {
 	
+	var vizApp = this;
+	
 	var vizData;
 	
 	var arrayOfNotes = [];
 	
-	var folderNameH6, vizContainer
+	var folderNameH6, vizContainer, colorButtons, layoutButtons, saveLayoutButton;
 	
 	var init = function() {
 		folderNameH6 = $('.js-group-name');
 		vizContainer = $('.js-vizContainer');
+		colorButtons = $('.coloring li');
+		layoutButtons = $('.positioning .layout');
+		saveLayoutButton = $('li[data-action="save_custom"]').hide()	
 	}
 	
 	var setData = function(data) {
@@ -18,35 +23,113 @@ var visualization = function() {
 	
 	var startViz = function() {
 		console.log(vizData);
-		vizContainer.fadeIn();
 		
-		vizData.folderSpecificAnnotations.map(createNote)
+		vizContainer.fadeIn();
+		vizData.folderSpecificAnnotations.map(createNote);
+		
+		colorButtons.on('click', function() {
+			var cat = $(this).attr('data-color')
+			console.log(cat);
+			colorNotes(cat);
+		});
+		
+		layoutButtons.on('click', function() {
+			var cat = $(this).attr('data-layout')
+			console.log(cat);
+			rearrangeNotes(cat);
+		});
+		
+		saveLayoutButton.on('click', saveCustomLayout)
 	}
 	
 	var createNote = function(noteObj) {
-		var newNotes = new Note(noteObj);
-		newNotes.setBackground(
+		var newNote = new Note(noteObj, vizApp, arrayOfNotes.length-1);
+		newNote.setBackground(
 			colorArray[Math.floor(Math.random()*colorArray.length)]);
-		arrayOfNotes.push(newNotes);
+		arrayOfNotes.push(newNote);
 	}
 	
+	
+	var rearrangeNotes = function(arrangement) {
+		if(arrangement == "custom") {
+			arrayOfNotes.map(function(note) {
+				var pos = note.customPosition();
+				note.position(pos);
+			});
+			saveLayoutButton.fadeOut();
+		} else {
+			arrayOfNotes.map(function(note) {
+				note.position([vizContainer.width()*Math.random(),vizContainer.height()*Math.random()]);
+			});
+		}
+	}
+	
+	var colorNotes = function(criteria) {
+		if(criteria != "") {
+			var arrayOfCriteria = generateArrayOfAttributes(criteria, arrayOfNotes);
+			arrayOfNotes.map(function(note) {
+				var attr = note.getNoteAttr(criteria);
+				note.setBackground(colorArray[arrayOfCriteria.indexOf(attr)])
+			});
+		} else {
+			arrayOfNotes.map(function(note) {
+				note.setBackground('#eee')
+			});
+		}
+	}
+	
+	var saveCustomLayout = function() {
+		arrayOfNotes.map(function(note) {
+			var pos = note.position();
+			note.customPosition(pos);
+		});
+		saveLayoutButton.fadeOut();
+	}
+	
+	this.showSaveLayoutButton = function() {
+		console.log('save button')
+		saveLayoutButton.fadeIn();
+	}
+
 	return {
 		setData : setData,
 		init : init
 	}
 }
 
+
+function generateArrayOfAttributes(criteria, arrayOfNotes) {
+	var rawArray = arrayOfNotes.map(function(note) {
+		return note.getNoteAttr(criteria);
+	});
+	
+	return rawArray.getUnique();
+}
+
+//http://stackoverflow.com/questions/1960473/unique-values-in-an-array
+Array.prototype.getUnique = function(){
+   var u = {}, a = [];
+   for(var i = 0, l = this.length; i < l; ++i){
+      if(u.hasOwnProperty(this[i])) {
+         continue;
+      }
+      a.push(this[i]);
+      u[this[i]] = 1;
+   }
+   return a;
+}
+
 var colorArray = 
 [
-	"#EDDEDE",
-	"#EDE3DE",
-	"#EDE8DE",
-	"#EDEDDE",
-	"#E6EDDE",
-	"#E0EDDE",
-	"#DEEDE0",
-	"#DEEDE6",
-	"#DEEDEB",
-	"#DEEBED",
-	"#DEE6ED"
+	"#E1BEE7",
+	"#D1C4E9",
+	"#C5CAE9",
+	"#BBDEFB",
+	"#B2EBF2",
+	"#DCEDC8",
+	"#FFECB3",
+	"#D7CCC8",
+	"#CFD8DC",
+	"#FFCDD2",
+	"#F8BBD0"
 ]
